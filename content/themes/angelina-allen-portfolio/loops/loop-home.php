a33cfacb8b95959db625c05676ef421d98deacdc
John Heimkes IV <john@markupisart.com>
2013-08-22 19:49:24 -0500

    began wrking on the post page

--- content/themes/angelina-allen-portfolio/loops/loop-home.php
+++ content/themes/angelina-allen-portfolio/loops/loop-home.php
@@ -8,11 +8,13 @@ $work_query = new WP_Query(array(
 if ( $work_query->have_posts() ) : while ( $work_query->have_posts() ) : $work_query->the_post(); ?>
 
     <div class="work-wrapper">
-        <?php the_post_thumbnail( 'work-featured' ); ?>
-        <h3 class="hdg-0 mix-hdg-white hdg-center hdg-center-white title-hover">
-            <span><?php the_title(); ?></span>
-        </h3>
-        <div class="bg-transparent"></div>
+        <a class="work-wrapper-link" href="<?php the_permalink(); ?>">
+            <?php the_post_thumbnail( 'work-featured' ); ?>
+            <h3 class="hdg-0 mix-hdg-white hdg-center hdg-center-white title-hover">
+                <span><?php the_title(); ?></span>
+            </h3>
+            <div class="bg-transparent"></div>
+        </a>
     </div>
 
 <?php endwhile; endif; ?>
