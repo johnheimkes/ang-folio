6a145dbcc1a735d84193230cce735b44ad9786e0
John Heimkes IV <john@johnheimkes.com>
2013-09-17 20:14:34 -0500

    finished link on work detail?

--- content/themes/angelina-allen-portfolio/loops/loop-single.php
+++ content/themes/angelina-allen-portfolio/loops/loop-single.php
@@ -28,7 +28,7 @@
             
             <div class="grid-col grid-col-2">
                 <h3 class="hdg-2 mix-hdg-salmon mix-hdg-border-salmon hdg-italic">Link :</h3>
-                <a href="<?php the_permalink(); ?>"><?php the_field( 'link' ); ?></a>
+                <a href="http://<?php the_field( 'link' ); ?>"><?php the_field( 'link' ); ?></a>
             </div>
             
             <div class="project-details cboth">
